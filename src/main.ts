299326c48564a222fa4c908e0eee6ce2bc721f53
Chirag Tagadiya <chiragtagadiya@Chirags-MacBook-Pro.local>
2025-09-22 13:28:27 -0500

    basic ts

---
+++ src/main.ts
@@ -0,0 +1,12 @@
+let username = "Mitesh"
+console.log(username)
+
+let a :number = 12;
+let b = '6'
+let c:number = 2
+console.log(a+b+c)
+
+console.log(a + b + c);
+//# sourceMappingURL=main.js.map
+b=6
+console.log(a/b)
