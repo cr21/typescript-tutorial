bd50d31803c806626d915b48028cc6bc795baa2e
Chirag Tagadiya <chiragtagadiya@Chirags-MacBook-Pro.local>
2025-09-26 18:01:13 -0500

    generics

--- src/main.ts
+++ src/main.ts
@@ -1,81 +1,148 @@
-const stringEcho = (arg:string):string => arg
+// utility type
 
-const echo = <T>(arg:T):T => arg
+// partial
 
-const isObj =<T>(arg:T):boolean => {
-    return typeof arg === 'object' && !Array.isArray(arg) && arg !== null
-} 
+interface Assignment{
+    studentId: string,  
+    title: string,
+    grade: number,
+    verified?: boolean
 
-const isTrue = <T>(arg:T):{arg:T,is:boolean} => {
-    if (Array.isArray(arg) && !arg.length) {
-        return {arg:arg,is:false}
-    }
-    if (isObj(arg) && !Object.keys(arg as keyof T).length) {
-        return {arg:arg,is:false}
-    }
-    return {arg:arg,is:!!arg}
 }
 
-console.log(isTrue(true))
-console.log(isTrue(false))
-console.log(isTrue(0))
-console.log(isTrue(1))
-console.log(isTrue(''))
-console.log(isTrue('test'))
-console.log(isTrue([]))
-console.log(isTrue([1,2,3]))
-console.log(isTrue({}))
-console.log(isTrue({name: 'John'}))
-console.log(isTrue(null))
-console.log(isTrue(undefined))
-console.log(isTrue(new Date()))
-console.log(isTrue(new Error()))
-console.log(isTrue(new Promise((resolve, reject) => resolve('test'))))
-console.log(NaN)
-console.log(isTrue(NaN))
-
-
-interface BoolCheck<T> {
-    value:T,
-    is:boolean
+const updateAssignment = (assign:Assignment,propsToUpdate:Partial<Assignment>):Assignment => {
+    return {...assign, ...propsToUpdate}
 }
 
-const checkBoolValue = <T>(arg:T):BoolCheck<T> => {
-    if (Array.isArray(arg) && !arg.length) {
-        return {value:arg,is:false}
-    }
-    if (isObj(arg) && !Object.keys(arg as keyof T).length) {
-        return {value:arg,is:false}
-    }
-    return {value:arg,is:!!arg}
+const assign1:Assignment = {
+    studentId: '1',
+    title: 'Math',
+    grade: 90,
+    verified: false
 }
 
-console.log(checkBoolValue(true))
-console.log(checkBoolValue(false))
-console.log(checkBoolValue(0))
-console.log(checkBoolValue(1))
-console.log(checkBoolValue(''))
-console.log(checkBoolValue('test'))
+const assign2:Assignment = updateAssignment(assign1,{grade:80})
+console.log(assign2)
 
-interface HasId {
-    id: number
+const assign3:Assignment = updateAssignment(assign1,{grade:80,verified:true})
+console.log(assign3)
+
+const assign4:Assignment = updateAssignment(assign1,{grade:80,verified:true})
+console.log(assign4)
+
+const assign5:Assignment = updateAssignment(assign1,{grade:80,verified:true})
+console.log(assign5)
+
+
+const hexColorMap:Readonly<Record<string,string>> = {
+    red: 'FF0000',
+    green: '00FF00',
+    blue: '0000FF'
 }
 
-const processUser = <T extends HasId>(user:T):T => {
-    return user
+console.log(hexColorMap.red)
+console.log(hexColorMap.green)
+console.log(hexColorMap.blue)
+// hexColorMap.red='000000'
+console.log(hexColorMap.red)
+
+type Students = 'John' | 'Jane' | 'Jim' | 'Jill'
+type LetterGrades = 'A' | 'B' | 'C' | 'D' | 'F'
+
+
+const finalGrades:Record<Students,LetterGrades> = {
+    John: 'A',
+    Jane: 'B',
+    Jim: 'C',
+    Jill: 'F'
 }
 
-console.log(processUser({id: 1, name: 'John'}))
-console.log(processUser({name: 'John','id':2,'email':'john@gmail.com'}))
 
-
-const getUserProperty = <T extends HasId, k extends keyof T>(users:T[],key:k) : T[k][]=>{
-    return users.map(user => user[key])
+interface Grades {
+    assign1: number,
+    assign2: number
 }
 
-console.log(getUserProperty([{id: 1, name: 'John'},{id: 2, name: 'Jane',email:'john@gmail.com'}],'email'))
-console.log(getUserProperty([{id: 1, name: 'John'},{id: 2, name: 'Jane'}],'id'))
-// console.log(getUserProperty([{id: 1, name: 'John'},{id: 2, name: 'Jane'}],'email'))
-console.log(getUserProperty([{id: 1, name: 'John'},{id: 2, name: 'Jane'}],'name'))
-console.log(getUserProperty([{id: 1, name: 'John'},{id: 2, name: 'Jane'}],'id'))
-// console.log(getUserProperty([{id: 1, name: 'John'},{id: 2, name: 'Jane'}],'email'))
+const gradeData :Record<Students,Grades> = {
+    John: {assign1: 10, assign2: 20},
+    Jane: {assign1: 20, assign2: 30},
+    Jim: {assign1: 30, assign2: 40},
+    Jill: {assign1: 40, assign2: 50}
+}
+
+console.log(gradeData.John.assign1)
+console.log(gradeData.Jane.assign1)
+console.log(gradeData.Jim.assign1)
+console.log(gradeData.Jill.assign1)
+
+
+const gradeDataByStudent = Object.values(gradeData).map(grade => grade.assign1 + grade.assign2)
+console.log(gradeDataByStudent)
+
+
+
+// pick and omit
+
+
+type AssignResult = Pick<Assignment,'studentId' | 'grade'>
+
+
+const score:AssignResult = {
+    studentId: '1',
+    grade: 90
+}
+
+console.log(score)
+
+type assignPreview = Omit<Assignment,'grade' | 'verified'>
+
+const preview:assignPreview = {
+    studentId: '1',
+    title: 'Math'
+}
+
+console.log(preview)
+
+
+/// Exclude and Extract
+
+
+type adjustedGradeResult = Exclude<LetterGrades,'F'>
+
+const adjustedGrades:Record<Students,adjustedGradeResult> = {
+    John: 'A',
+    Jane: 'B',
+    Jim: 'C',
+    Jill: 'A'
+}
+
+console.log(adjustedGrades)
+
+type highGrades = Extract<LetterGrades,'A' | 'B'>
+
+const highGrades:Record<Students,highGrades> = {
+    John: 'A',
+    Jane: 'B',
+    Jim: 'C',
+    Jill: 'A'
+}
+
+console.log(highGrades)
+
+// Non Nullable
+
+type names = 'John' | 'Jane' | 'Jim' | 'Jill' | null | undefined
+
+const nonNullableNames:NonNullable<names> ='Jill'
+
+console.log(nonNullableNames)
+
+// Return Type
+
+type MaybeUser = {name:string} | undefined
+
+const maybeUser:MaybeUser = {name: 'John'}
+
+console.log(maybeUser)
+
+// Return Type
